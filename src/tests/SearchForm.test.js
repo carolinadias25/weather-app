62510b10d33d67e87efb838367ebfe8523ad6731
carolinadias25 <carolina.f.dias@hotmail.com>
2021-06-23 21:00:02 +0100

    final update - all tests passing

--- src/tests/SearchForm.test.js
+++ src/tests/SearchForm.test.js
@@ -4,7 +4,7 @@ import SearchForm from "../components/SearchForm";
 
 describe("SearchForm", () => {
   it("renders correctly", () => {
-    const { getByText } = render(<SearchForm />);
+    const { getByText } = render(<SearchForm searchText={''} setSearchText={jest.fn()} onSubmit={jest.fn}/>);
     expect(getByText("Search")).toHaveClass("search-form__button");
   });
 });
